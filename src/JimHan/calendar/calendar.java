512153bd0b5908b2ed01515331483eb3400ffb8c
JimHan93 <jim.jinmo.han@gmail.com>
2022-09-10 09:08:24 -0400

    Update calendar.java
    Simplified the main method by using classes

--- src/JimHan/calendar/calendar.java
+++ src/JimHan/calendar/calendar.java
@@ -1,23 +1,35 @@
 package JimHan.calendar;
+
+
 import java.util.Scanner;
 
 public class calendar {
-	public static void main(String[] args) {
-		int month;
-		int[] days= {31, 28, 31, 30, 31, 30, 31,
-		             31, 30, 31, 30, 31};
-		
-		System.out.println(" S  M  Tu W  Th F  S");
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31,
+            31, 30, 31, 30, 31};
+	
+	public static int maxDaysofMonths(int month) {
+		return MAX_DAYS[month-1];
+	}
+	
+	public void sampleCalendar() {
+		System.out.println(" S  M Tu  W Th  F  S");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+	}
 	
-		System.out.println("Please input the number of the month");
+	public static void main(String[] args) {
+		int month;
+		int maxDays;
 		Scanner scanner = new Scanner (System.in);
+		
+		System.out.println("Please input the number of the month");
 		month = scanner.nextInt();
-		System.out.printf("%d has %d days", month,days[month-1]);
+		
+		System.out.printf("%d has %d days\n",month,maxDaysofMonths(month));
+		scanner.close();
 		
 	}
 	
